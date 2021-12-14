9cc62b350c448209dce19e6cd7530e3e5ada3cb4
ainoylaoutinen <bgp962@myy.haaga-helia.fi>
2021-12-14 16:08:31 +0200

    final

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
-import _ from "lodash";
 
 export default function Statistics() {
   const [values, setValues] = useState([
